52dbcb3159b87a80a801db764ed6eec5f88c3158
Lunosat <akirah.ofc@gmail.com>
2023-02-10 23:00:17 -0300

    fu

--- saque.js
+++ saque.js
@@ -2,3 +2,9 @@ const hover = () => {
     this.classList.toggle('clicked');
 }
 
+const saque = () => {
+    const btn = document.getElementById('sacado')
+    //btn.style.display  = block
+    btn.innerHTML = 'Parabéns Marcio Alves, seu saque via pix foi aprovado! Em até 24 horas estará disponível em sua conta!'
+}
+
